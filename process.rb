488ab32451d9928477d0895df4bd2e179abb355b
Petru Madar <petrumadar@yahoo.com>
2016-01-11 09:34:43 +0000

    Ignore Thumbs.db

--- process.rb
+++ process.rb
@@ -2,9 +2,10 @@
 require './bzmark'
 files = Dir.entries("input/",{encoding: "utf-8"}).select {|f| !File.directory? f}
 files.each { |file|
-    img = Bzmark.new(file)
-    img.process
-    puts "Processing #{file}..."
+  next if file == "Thumbs.db"
+  img = Bzmark.new(file)
+  img.process
+  puts "Processing #{file}..."
 }
 puts "Done. Press ENTER to exit."
 gets
